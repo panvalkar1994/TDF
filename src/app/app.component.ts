6e405077b45911db258b8c67e69e00417c196738
vaibhav <vpanvalkar@outlook.com>
2021-03-27 18:49:24 +0530

    two way binding with default instance of spartan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Spartan } from './spartan';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TDF';
+  spartanModel = new Spartan('Rob','rob@bank.com','777770000',true);
 }
