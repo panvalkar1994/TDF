ba91978f72479d9847d41ab7fbef766a161a88e2
vaibhav <vpanvalkar@outlook.com>
2021-03-28 17:29:41 +0530

    enrollement service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 import { Spartan } from './spartan';
 
 @Component({
@@ -9,4 +10,14 @@ import { Spartan } from './spartan';
 export class AppComponent {
   title = 'TDF';
   spartanModel = new Spartan('Rob','rob@bank.com','777770000',true);
+
+  constructor(private _enrollmentService:EnrollmentService){}
+
+  onSubmit(){
+    this._enrollmentService.enroll(this.spartanModel)
+      .subscribe(
+        data=> console.log('Success!', data),
+        error=> console.log('error',error)
+      )
+  }
 }
